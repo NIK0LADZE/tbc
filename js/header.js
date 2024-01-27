3fdb6c5044e55f814d2a7caf18138940558ef0d1
NIK0LADZE <giorgi.nikoladze@scandiweb.com>
2024-01-27 05:39:14 +0400

    mobile header show/hide implemented

--- js/header.js
+++ js/header.js
@@ -2,7 +2,25 @@ const burgerIcon = document.querySelector('.burger-icon');
 const closeIcon = document.querySelector('.close-icon');
 const menuOverlay = document.querySelector('.menu-overlay');
 const menuNav = document.querySelector('nav');
+const header = document.querySelector('header');
 let isMenuOpened = false;
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && scrollTop > 50) {
+        if (!header.classList.contains('isHeaderHidden')) {
+            header.classList.add('isHeaderHidden');
+        }
+    } else {
+        if (header.classList.contains('isHeaderHidden')) {
+            header.classList.remove('isHeaderHidden');
+        }
+    }
+
+    lastScrollTop = scrollTop;
+});
 
 const openOrCloseMenu = () => {
     if (!isMenuOpened) {
